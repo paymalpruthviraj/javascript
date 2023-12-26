dedcbc5fe1b765e918f516b63d8bd5483a3cf44b
paymalpruthviraj <153293943+paymalpruthviraj@users.noreply.github.com>
2023-12-26 18:08:03 +0530

    programs
    programs

---
+++ helloWorld.js
@@ -0,0 +1,8 @@
+console.log("hello Js' I am Pruthviraj");
+var num = "10";
+console.log(num);
+var myName = "pruthviraj";
+console.log(myName);
+console.log("First Name",myName);
+var myName = "pruthviraj paymal";
+console.log("Full name",myName);
