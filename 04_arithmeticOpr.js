6ef0ed0805aa5a445f7d0696b0b6321d70dbdb22
paymalpruthviraj <153293943+paymalpruthviraj@users.noreply.github.com>
2023-12-30 22:17:47 +0530

    30-Dec-attendence

---
+++ 04_arithmeticOpr.js
@@ -0,0 +1,23 @@
+var n1= 7;
+var n2 = 2;
+
+var result = n1 + n2;
+console.log(` + ==>  Addition is : ${result}`);
+
+var result = n1 - n2;
+console.log(` - ==>  Substraction is : ${result}`);
+
+var result = n1 * n2;
+console.log(` * ==>  Multiplication is : ${result}`);
+
+var result = n1 % n2;
+console.log(` % ==>  Modulus or Reminder is : ${result}`);
+
+var result = 1280 % 2;
+console.log(` % ==>  Modulus or Reminder is : ${result}`);
+
+var result = n1 / n2;
+console.log(` / ==>  Division is : ${result}`);
+
+var result = n1 ** 3;
+console.log(` ** ==>  Exponention is : ${result}`);
