7143657feffc14cad33337599851d38ec8ef860c
paymalpruthviraj <153293943+paymalpruthviraj@users.noreply.github.com>
2024-01-24 20:54:01 +0530

    attdence 23-Jan-24 and 24-Jan-24

--- dom/14_main.js
+++ dom/14_main.js
@@ -1,7 +1,6 @@
 var element = document.getElementById('elementId');
 element.style.color = 'Orange';
 
-// const projectElement = document.querySelector('#projectName');
-// projectElement.innerHTML = "Project Title: College Management System";
+
 const projectDescription = document.querySelector('#projectName');
 projectDescription.innerHTML = "Project Description : developing a basic web-based task manager using HTML,css, and javascript";
