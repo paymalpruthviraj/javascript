14daeddf0ffa7b71e7765ddc0942f4ced8975613
paymalpruthviraj <153293943+paymalpruthviraj@users.noreply.github.com>
2024-02-17 20:34:29 +0530

    .

--- new.js
+++ new.js
@@ -1,16 +1,17 @@
-const isNotesAvailable = true;
-const promiseNotes = new Promise((success,reject)=>{
-    if(isNotesAvailable){
-        success("Sharing the notes..Here you go..");
-    }
-    reject(`Sorry guys..I didn't get time`);
-});
-promiseNotes.then(success=>{
-    console.log(success);
-    console.log(`Thank you Master`);
-}).catch(failure=>{
-    console.log(failure);
-    console.log(`very kadhus master..`);
-}).finally(()=>{
-    console.log(`I must have my notes...`);
-})
+const arrayRollNumber = [113,45,56,11,32,45,109,799,56,45];
+
+const reverse = arrayRollNumber.reverse();
+console.log(reverse);
+
+//use the sort() method as it is without any custom sorting (without passing any argument) and notice the issue
+
+const sort = arrayRollNumber.sort();
+console.log(sort);
+
+//sort the array in ascending order by writing your custorm logic
+
+const acending = arrayRollNumber.sort((a,b)=>{return a>b ? 1: -1});
+console.log(acending);
+
+//find the Greatest number from the array
+const 
