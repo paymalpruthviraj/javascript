71b013f59c0896d0a7b3bb20d63c4d8a98b43c9c
paymalpruthviraj <153293943+paymalpruthviraj@users.noreply.github.com>
2024-02-21 19:57:40 +0530

    .

--- new.js
+++ new.js
@@ -1,17 +1,4 @@
-const arrayRollNumber = [113,45,56,11,32,45,109,799,56,45];
-
-const reverse = arrayRollNumber.reverse();
-console.log(reverse);
-
-//use the sort() method as it is without any custom sorting (without passing any argument) and notice the issue
-
-const sort = arrayRollNumber.sort();
-console.log(sort);
-
-//sort the array in ascending order by writing your custorm logic
-
-const acending = arrayRollNumber.sort((a,b)=>{return a>b ? 1: -1});
-console.log(acending);
-
-//find the Greatest number from the array
-const 
+const array = [10,20,30,40,50,60,70];
+array.forEach(element => {
+    console.log([index]);
+});
