e3ab48e82ad1d8ef2e366da9492b8e6aba6ef418
paymalpruthviraj <153293943+paymalpruthviraj@users.noreply.github.com>
2024-02-23 20:45:11 +0530

    .

--- new.js
+++ new.js
@@ -1,4 +1,28 @@
-const array = [10,20,30,40,50,60,70];
-array.forEach(element => {
-    console.log([index]);
-});
+const arrayNumber = [20,11,40,25,37,49,9,90,60,2,19];
+
+const greather = arrayNumber.filter((element)=>{
+  return element>50;
+})
+console.log(greather);
+
+const even = arrayNumber.filter((element)=>{
+  return element%2==0;
+})
+console.log(even);
+
+const odd = arrayNumber.filter((element)=>{
+  return element%2!=0;
+})
+console.log(odd);
+
+
+const mult = arrayNumber.filter((element)=>{
+  return element%5==0;
+})
+console.log(mult);
+
+const between = arrayNumber.filter((element)=>{
+  return element>20 && element<50;
+})
+console.log(between);
+
