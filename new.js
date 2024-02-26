75d3ace1e933bd8c7aaac7fcce89e61f9df9c574
paymalpruthviraj <153293943+paymalpruthviraj@users.noreply.github.com>
2024-02-26 19:36:57 +0530

    .

--- new.js
+++ new.js
@@ -1,28 +1,7 @@
-const arrayNumber = [20,11,40,25,37,49,9,90,60,2,19];
+const arrayRollNumbere = [113,45,56,11,32,45,109,799,56,45];
 
-const greather = arrayNumber.filter((element)=>{
-  return element>50;
+arrayRollNumbere.sort((b,a)=>{
+return b>a ? -1:1;
 })
-console.log(greather);
-
-const even = arrayNumber.filter((element)=>{
-  return element%2==0;
-})
-console.log(even);
-
-const odd = arrayNumber.filter((element)=>{
-  return element%2!=0;
-})
-console.log(odd);
-
-
-const mult = arrayNumber.filter((element)=>{
-  return element%5==0;
-})
-console.log(mult);
-
-const between = arrayNumber.filter((element)=>{
-  return element>20 && element<50;
-})
-console.log(between);
+console.log(arrayRollNumbere);
 
